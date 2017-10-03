2c07255ba7c57ebcd02e016ebc9aaee853ea4ef2
Brian Jorda <brianjorda@gmail.com>
2017-10-03 03:54:36 -0300

    Mejor forma de hacer el include.

--- core/config.php
+++ core/config.php
@@ -4,5 +4,5 @@ $link = mysqli_connect("localhost","root","","netcomics") or die("Error " . mysq
 $site_url = 'http://localhost/netcomics/';
 $relative_folder = '/netcomics';
 //set_include_path(get_include_path() . PATH_SEPARATOR . 'C:\xampp\htdocs\netcomics');
-include '../lang/views_es.php';
+include(dirname(__DIR__).'/lang/views_es.php');
 ?>
